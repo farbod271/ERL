36757e06722e217982d2f5a10276428cc02f0f39
farbod271 <farodmatin@gmail.com>
2023-10-28 03:50:02 +0400

    testing commit

--- ERL.py
+++ ERL.py
@@ -1,21 +1,79 @@
-# read this json link
 import json
-url  = "https://www.erl.de/wp-json/kundenportal/v1/onoffice/get-stammobjekt-free-apartments?id=573"
+from urllib.request import urlopen
+import time
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-from urllib.request import urlopen 
-  
-# store the response of URL 
-response = urlopen(url) 
-  
-# storing the JSON response  
-# from url in data 
-data_json = json.loads(response.read()) 
-  
-# print the json response 
-# if ((data_json["success"]) == True) and (data_json["apartmentsAvailable"]["0"] = 0) and (data_json["apartmentsAvailable"]["1"] = 0) and (data_json["apartmentsAvailable"]["2"] = 0) and (data_json["apartmentsAvailable"]["3"] = 280):
-#     print("No apartments available")
+active = True
+async def job(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        url  = "https://www.erl.de/wp-json/kundenportal/v1/onoffice/get-stammobjekt-free-apartments?id=573"
 
-if (data_json["success"] == True) and data_json["apartmentsAvailable"]["0"] == 0 and (data_json["apartmentsAvailable"]["1"] == 0) and (data_json["apartmentsAvailable"]["2"] == 0) and (data_json["apartmentsAvailable"]["3"] ==280):
-        print("No apartments available")
-else:
-        print("apartments available")
+        # counter = 0
+        response = urlopen(url)
+        data_json = json.loads(response.read())
+
+        try:
+                if (data_json["success"] == True) and data_json["apartmentsAvailable"]["0"] == 0 and (data_json["apartmentsAvailable"]["1"] == 0) and (data_json["apartmentsAvailable"]["2"] == 0) and (data_json["apartmentsAvailable"]["3"] ==280):
+                        # counter += 1
+                        # if counter > 60:
+                        await update.message.reply_text("No apartments available")
+                        print("No apartments available")
+                                # counter = 0
+                else:
+                        await update.message.reply_text("No apartments available")
+                        print("No apartments available")
+        except:
+                print("Error")
+                active = True
+        await update.message.reply_text("jobbing!")
+        print("Bot started")
+
+
+
+
+
+
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("job", job))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+
+if __name__ == "__main__":
+    main()
+    while active == True:
+        job()
+        time.sleep(5)
