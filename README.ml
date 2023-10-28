29571975982e73e93a3cc16dc058d97f98c87549
farbod271 <farodmatin@gmail.com>
2023-10-28 18:39:37 +0400

    edited readme file

--- README.ml
+++ README.ml
@@ -1,58 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
+# Apartment Availability Checker
 
-        #container {
-            background-color: #ffffff;
-            margin: 20px;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+This script fetches information about available apartments from a specific link and determines if apartments in ERL Deggendorf are currently available or not. For security reasons, I have excluded the bot token from the Git repository.
 
-        h1 {
-            color: #333;
-        }
+If you wish to use this script with your own bot, simply replace the `variables.token` with your bot's token.
 
-        p {
-            font-size: 18px;
-            line-height: 1.5;
-        }
+**Note**: If you are from ERL and have concerns about this repository, please send me a message, and I will take appropriate action, including removing the repository if necessary.
 
-        code {
-            background-color: #f0f0f0;
-            border: 1px solid #ccc;
-            padding: 2px 5px;
-            border-radius: 3px;
-        }
-    </style>
-</head>
-<body>
-    <div id="container">
-        <h1>Apartment Availability Checker</h1>
+## Getting Started
 
-        <p>This script fetches information about available apartments from a specific link and determines if apartments in ERL Deggendorf are currently available or not. For security reasons, I have excluded the bot token from the Git repository.</p>
+To get started, follow these steps:
 
-        <p>If you wish to use this script with your own bot, simply replace the <code>variables.token</code> with your bot's token.</p>
+1. Clone this repository to your local machine.
+2. Replace `variables.token` with your bot's token in the script.
+3. Run the script to check apartment availability.
 
-        <p><strong>Note:</strong> If you are from ERL and have concerns about this repository, please send me a message, and I will take appropriate action, including removing the repository if necessary.</p>
+## Contact
 
-        <h2>Getting Started</h2>
-        <ol>
-            <li>Clone this repository to your local machine.</li>
-            <li>Replace <code>variables.token</code> with your bot's token in the script.</li>
-            <li>Run the script to check apartment availability.</li>
-        </ol>
-
-        <h2>Contact</h2>
-        <p>If you have any questions or concerns, feel free to reach out by sending a message.</p>
-    </div>
-</body>
-</html>
+If you have any questions or concerns, feel free to reach out by sending a message.
